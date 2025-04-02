ce3745bca5f10c992b2c45963ee78e77de4bc1dc
FzeroWCS <57687346+FzeroWCS@users.noreply.github.com>
2025-04-02 01:33:31 -0300

    3ª commit-adicionando comando printf

---
+++ basicC/app.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+   printf("%d\n",'a');
+   printf("%d\n",'b');
+   printf("%d\n","ab");
+}
+
---
+++ pietroY/aula2_1/aula2ep1.c
@@ -0,0 +1,7 @@
+//funçaõ printf() ara exibir mensagens
+//na saída padão
+#include <stdio.h>
+int main(void){ 
+   printf("Ola mundo");
+   return 0;
+}
