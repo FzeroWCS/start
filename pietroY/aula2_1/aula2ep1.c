e7a715e2892ee528ee61e1078d9885c0e835b81b
FzeroWCS <wagnercezarsilva@gmail.com>
2025-04-09 16:05:26 -0300

    10 commit- função printf();

--- pietroY/aula2_1/aula2ep1.c
+++ pietroY/aula2_1/aula2ep1.c
@@ -2,9 +2,14 @@
 //na saída padão
 #include <stdio.h>
 int main(void){ 
-    printf("\033c");
-   printf("Ola mundo");
-   printf("\n");
-   puts("Impressão de texto com puts()");
-   return 0;
+
+    printf("\033c");//limpar a tela
+    printf("Ola mundo");
+    printf("\%d numero inteiro %d",10);
+    printf("\%i numero inteito %i", 10);
+    printf("\%X número inteiro base Hexa %X",10);
+    printf("\%F número ponto flutuante %f",10);
+    printf("\%e número em notação científica %e",10);
+    printf("\%c caractéres alfanúmericos (texto) %c",'d');
+    return 0;
 }
